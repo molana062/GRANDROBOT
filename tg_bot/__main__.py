30b815254a964e7526353ceee4b679ce5ada624b
molana062 <79923200+molana062@users.noreply.github.com>
2021-03-26 05:31:53 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -26,9 +26,9 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 
 PM_START_TEXT = """
-**Hello {}, My Name is {}!** 
-I am an **SUPERB**  group management bot.
-You can find the list of available commands with /help.
+**Aloo {}, Aku adalah {}!** 
+Aku Adalah Bot Manajemen Group, Bersedia Untuk Membasi Orang-Orang Tolol Di Telegram.
+Ketik help untuk melihat beberapa fungsi /help.
 
 """
 
@@ -49,7 +49,7 @@ Hello! my name *{}*.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/84b2017bc2f3c90f2e61c.jpg"
+TECHNO_IMG = "https://telegra.ph/file/ef0b5ae50b5097ea19502.jpg"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
@@ -141,12 +141,12 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/teamishere")],
-                                                                                   [InlineKeyboardButton(text="ADD GRAND OFFICIAL TO YOUR GROUP",
+                                                                                   InlineKeyboardButton(text="Owner",
+                                                                       url="t.me/Betterthaanhecan")],
+                                                                                   [InlineKeyboardButton(text="ADD AUZEA TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/legendx22/GRANDROBOT")
+                                                                                   InlineKeyboardButton(text="Channel Owner",
+                                                                       url="t.me/ruangpublikk")
                                                                                  ]]))
 
     else:
